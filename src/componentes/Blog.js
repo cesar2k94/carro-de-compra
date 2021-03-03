d466b6d1030d41ba33cf9ca4bc36469341296d4a
Julio <juliomederosarias@gmail.com>
2021-03-03 15:20:55 -0300

    Creando interfaz

---
+++ src/componentes/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return ( 
+        <div>
+            <h1>Blog</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.</p>
+        </div>
+     );
+}
+ 
+export default Blog;
